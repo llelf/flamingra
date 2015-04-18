f292b052da55526d57109bbd3e8f90771b1fe3b0
Antonio Nikishaev <me@lelf.lu>
2015-04-18 21:31:47 +0000

    working rewrite

--- Main.hs
+++ Main.hs
@@ -1,64 +1,80 @@
-{-# LANGUAGE MultiWayIf #-}
 module Main where
 
 import Text.Printf
 import Data.List
-import qualified Pipes.Prelude as P
 import Pipes
-import Pipes.Lift
-import qualified Control.Foldl as F
-import qualified Pipes.Extras as PE
-import Control.Applicative
+import qualified Pipes.Prelude as P
 import Control.Monad
 import Options.Applicative
 import System.IO
+import System.Exit
 import Control.Monad.Trans.Either
-import Control.Monad.Trans.State.Strict hiding (State)
+
 
 
 type State = ([String], Int)
-type Foldee = (State,String)
+type Foldee = (State, Maybe String)
 
-fld :: Foldee -> String -> Foldee
-fld (state@(stack,prevN),res) str
-    | ticks>0   = ((stack',n), printf "%s %d" (intercalate ";" stack') ticks)
-    | otherwise = (state, res)
+-- COST CENTRE    ticks
+-- A       …      n₁                     A n₁
+--  B      …      n₂            ===>     A;B n₂
+--  C      …      n₃                     A;C n₃
+-- 
+conv :: Foldee -> String -> Foldee
+conv (state@(stack,prevLevel),_) str
+    | length ws == 10 && ticks>0 = ((stack',level), Just $ printf "%s %d" (intercalate ";" stack') ticks)
+    | otherwise                  = (state,          Nothing)
     where 
       ws = words str
-      n = length $ takeWhile (==' ') str
+      level = length . takeWhile (==' ') $ str
       fun = printf "%s:%s" (ws!!1) (ws!!0)
       ticks = read (ws!!8) :: Int
-      stack' = take (length stack - (prevN-n+1)) stack ++ [fun]
+      stack' = take (length stack - (prevLevel-level+1)) stack ++ [fun]
 
 
-data Status = OK | PTooSmall | NotProf deriving Show
+data Status = OK | PTooSmall deriving Show
 
 
-work :: Effect (EitherT Status (StateT Status IO)) ()
-work = 
-       P.stdinLn
-           >-> P.dropWhile (\x -> not (isPrefixOf "COST CENTRE" x && isInfixOf "no." x))
+work :: Producer String (EitherT Status IO) ()
+     -> Producer String (EitherT Status IO) ()
+work input =
+     input >-> P.dropWhile (not . ((&&) <$> isPrefixOf "COST CENTRE"
+                                        <*> isInfixOf "no."))
+           >-> do line <- await
+                  when ("%alloc" `isSuffixOf` line) . lift $ left PTooSmall
+                  cat
+           >-> P.dropWhile null
+           >-> P.scan conv (([],0),Nothing) snd
+           >-> P.concat
+           >-> P.tee P.stdoutLn
 
-           >-> (do
-                 line <- await
-                 when ("%alloc" `isSuffixOf` line) $ lift $ left PTooSmall
-                 lift.lift $ put OK
-                 cat
-                 )
-            >-> P.stdoutLn
 
+
+data Options = Options {
+      inputFile :: Maybe String
+}
+
+options = Options <$> optional (argument str (metavar "PROF-FILE"))
 
 
 main :: IO ()
-main = do 
-  inp <- openFile "../pb.prof" ReadMode
+main = execParser opts >>= main'
+    where opts = info (helper <*> options) fullDesc
 
-  r <- flip runStateT NotProf $ 
-       runEitherT $ 
-       runEffect $ work
+
+main' :: Options -> IO ()
+main' opts = do
+  inp <- case inputFile opts of
+           Just file -> P.fromHandle <$> openFile file ReadMode
+           Nothing   -> return P.stdinLn
+
+  r <- runEitherT . P.length $ work inp
 
   case r of
-    (Left PTooSmall, _) -> putStrLn "-P, not -p"
-    (_, NotProf) -> putStrLn "doesn't look like a .prof"
-    (Right (), OK) -> return ()
+    Left PTooSmall -> die "Error: looks like it’s -p output, I need a -P one"
+    Right 0        -> die "Error: the input doesn't look like a .prof"
+    Right _        -> return ()
 
+
+die :: String -> IO ()
+die str = hPutStrLn stderr str >> exitWith (ExitFailure 1)
